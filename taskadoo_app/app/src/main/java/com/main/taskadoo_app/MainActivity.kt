8d42684636fbcc58eb7e615e4056162572e22c59
Radosław Wolnik <60506818+powolnik@users.noreply.github.com>
2025-02-22 08:34:48 +0100

    Jetpack familiarized, ive get some control over @Composable s

--- taskadoo_app/app/src/main/java/com/main/taskadoo_app/MainActivity.kt
+++ taskadoo_app/app/src/main/java/com/main/taskadoo_app/MainActivity.kt
@@ -1,37 +1,40 @@
 package com.main.taskadoo_app
 
+
 import android.os.Bundle
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.graphics.Color
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
-import androidx.compose.material3.Scaffold
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.unit.TextUnit
 import com.main.taskadoo_app.ui.theme.Taskadoo_appTheme
 
+var FontSize: TextUnit = 0.sp
+var FontSizeMuliplier: Float = 0.4F
+var DispText: String = "Default"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (FontSize <= 0.sp) {
+            DispText = "TextSize \n\nzainicjuj \n\nbalwanie"
+            FontSize = 90.sp
+        }
         enableEdgeToEdge()
         setContent {
             Taskadoo_appTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Column {
                     GreetingText(
                     )
                 }
@@ -40,42 +43,49 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Greeting MainActivity $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Taskadoo_appTheme {
-        Greeting("Android")
-    }
+fun GreetingTextPreview() {
+    GreetingText()
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingText() {
+fun Greeting(name: String = DispText, modifier: Modifier = Modifier) {
     Text(
         buildAnnotatedString {
-            withStyle(style = ParagraphStyle(lineHeight = 30.sp)) {
-                withStyle(style = SpanStyle(color = Color.Blue)) {
-                    append("x\nx\nx\n")
+            withStyle(style = ParagraphStyle(lineHeight = FontSize)) {
+                withStyle(style = SpanStyle(color = Color.Red, fontSize = FontSize)) {
+                    append("$name.")
                 }
-                withStyle(
-                    style = SpanStyle(
-                        fontWeight = FontWeight.Thin, color = Color.Red
-                    )
-                ) {
-                    append("s\n")
-                }
-                append("x\n")
             }
         }
     )
+}
+
+@Composable
+fun GreetingText(name: String = DispText) {
+    var FontColor: Color = Color.Cyan
+    Column {
+        Greeting("Radek")
+        Text(
+            buildAnnotatedString {
+                withStyle(style = ParagraphStyle(lineHeight = FontSize*FontSizeMuliplier)) {
+                    withStyle(
+                        style = SpanStyle(
+                            fontSize = FontSize/FontSizeMuliplier*FontSizeMuliplier,
+                            fontWeight = FontWeight.ExtraBold,
+                            color = FontColor
+                        )
+                    ) {
+                        if (FontWeight.equals(FontWeight.ExtraBold)) {
+                            FontColor = Color.Red
+                        }
+                        append("$name!")
+                    }
+                }
+            }
+        )
+    }
 }
 
 

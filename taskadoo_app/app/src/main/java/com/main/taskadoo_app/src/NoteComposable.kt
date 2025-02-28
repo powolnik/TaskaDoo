25cd3880452c67c3aa67b71a8bdb5e780b18d305
Radosław Wolnik <60506818+powolnik@users.noreply.github.com>
2025-02-28 01:22:27 +0100

    Rename Button.kt to BasicTypes.kt, create TDText composable with custom styling, update Note.kt to use clickable Card with title and content layout, modify NoteComposable to use TDButton, add deployment target for MainActivity in IDE settings

--- taskadoo_app/app/src/main/java/com/main/taskadoo_app/src/NoteComposable.kt
+++ taskadoo_app/app/src/main/java/com/main/taskadoo_app/src/NoteComposable.kt
@@ -8,7 +8,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.main.taskadoo_app.ui.theme.ComposeTestTheme
 
 
 @Composable
